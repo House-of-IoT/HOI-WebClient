21f0d9b862106955fca9ad91bc55122dd0110476
RonaldColyar <phantexcreations@gmail.com>
2021-05-13 02:27:01 -0500

    init for extra components

---
+++ Frontend/src/Components/ServerStatus.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ServerStatus extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
