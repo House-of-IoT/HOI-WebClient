f5b19a86582bd2ae90194aff113fd551393fb293
RonaldColyar <phantexcreations@gmail.com>
2021-05-12 20:11:50 -0500

    init listing hub

---
+++ Frontend/src/Components/ListingHub.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListingHub extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
