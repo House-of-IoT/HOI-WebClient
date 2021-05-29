5f7d371465925f413b918d3391b045e7e85b5853
RonaldColyar <phantexcreations@gmail.com>
2021-05-29 16:59:55 -0500

    new connections styling

---
+++ Frontend/src/Components/ConnectionsList.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Connections extends Component {
+    render() {
+        return (
+            <div id = "connections">
+                <h2 id = "connection-header">Connections<button id = "add-connections">+</button> </h2>
+                
+            </div>
+        )
+    }
+}
--- Frontend/src/Screens/MainHub.tsx
+++ Frontend/src/Screens/MainHub.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../Img/bot.png';
 import '../Css/main.css';
 import ListingHub from '../Components/ListingHub';
+import Connections from '../Components/ConnectionsList';
 export default class MainHub extends Component {
     render() {
         return (
@@ -10,7 +11,7 @@ export default class MainHub extends Component {
                 <h1>House Of IoT</h1>
                 <ListingHub/>
                 <input id = "bot-search" placeholder = "Search By Name"></input>
-                
+                <Connections/>
             </div>
         )
     }
