80e830aaa76da7795e4db639040216be23ffffab
RonaldColyar <phantexcreations@gmail.com>
2021-05-29 11:57:55 -0500

    init

---
+++ Frontend/src/Components/ConnectionListing.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ConnectionListing extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
