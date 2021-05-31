2cba621cc33ab8e92ed1067c7e5b76badf116324
RonaldColyar <phantexcreations@gmail.com>
2021-05-31 13:10:53 -0500

    minor config

--- Frontend/src/Components/ConfigHandler.tsx
+++ Frontend/src/Components/ConfigHandler.tsx
@@ -4,7 +4,11 @@ export default class ConfigHandler extends Component {
     render() {
         return (
             <div id = "config-handler">
-                
+                <h1>Manage Configuration</h1>
+                <button>Save</button>
+                <button>Insert New</button>
+                <button>Remove</button>
+               
             </div>
         )
     }
