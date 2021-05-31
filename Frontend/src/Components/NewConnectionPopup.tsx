8bb0d0de37d2fb957896fd453d52619a1fa86f81
RonaldColyar <phantexcreations@gmail.com>
2021-05-31 18:26:49 -0500

    added new pop up comps

---
+++ Frontend/src/Components/NewConnectionPopup.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewConnectionPopup extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
