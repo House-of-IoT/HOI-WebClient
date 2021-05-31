a7ad98d0fef47d95c43d0d067b1594b0369c85bc
RonaldColyar <phantexcreations@gmail.com>
2021-05-31 13:06:09 -0500

    init

---
+++ Frontend/src/Components/ConfigHandler.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ConfigHandler extends Component {
+    render() {
+        return (
+            <div id = "config-handler">
+                
+            </div>
+        )
+    }
+}
--- Frontend/src/Components/SnapShot.tsx
+++ Frontend/src/Components/SnapShot.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import greenicon  from "../Img/greenicon.png";
 export default class SnapShot extends Component<{bots:number,types:Set<String>},any> {
     render() {
         return (
             <div id = "snap-shot">
-             <h1>SnapShot <img></img></h1>
+             <h1>SnapShot <img id = "icon-snapshot" src = {greenicon}></img></h1>
              <h2>Total bots:{this.props.bots}</h2>
              <h2>Different types: {this.props.types.size}</h2>   
             </div>
