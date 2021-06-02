f1d0dc5780999ed57aae66c46c5cab62d69ffd5e
RonaldColyar <phantexcreations@gmail.com>
2021-06-01 22:02:16 -0500

    basic

--- Frontend/src/Components/RemovalConfirmPopup.tsx
+++ Frontend/src/Components/RemovalConfirmPopup.tsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 export default class RemovalConfirmPopup extends Component {
     render() {
         return (
-            <div>
+            <div className = "popup-wrapper">
+                <div id = "removal-popup">
+                    <h1>Are you sure you would like to remove this?</h1>
+                    <button>Yes</button>
+                    <button>No</button>
+                </div>
                 
             </div>
         )
