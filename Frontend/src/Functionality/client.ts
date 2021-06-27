76f700bde2c70f0a05b7893a40a3e47c400d2b4b
RonaldColyar <phantexcreations@gmail.com>
2021-06-27 01:18:58 -0500

    new client progression WIP

--- Frontend/src/Functionality/client.ts
+++ Frontend/src/Functionality/client.ts
@@ -8,6 +8,8 @@ export class Client{
     auth_status : Map<string,string>
     name_and_type : string //json string
     current_server_trying_to_auth :string
+    server_status : Map<string,Boolean>
+    set_bot_state:any
 
     constructor(name_and_type:string){
         this.name_and_type = name_and_type;
@@ -31,6 +33,7 @@ export class Client{
                 let connection :WebSocket = this.connections.get(server_name);
                 this.auth_status.set(server_name,"unknown");
                 this.current_server_trying_to_auth = server_name; 
+                connection.onmessage = this.handle_auth_response;
                 connection.send(password);
                 connection.send(this.name_and_type);
             }
@@ -46,13 +49,56 @@ export class Client{
     handle_auth_response(event:MessageEvent){
         if(event.data == "success"){
             this.auth_status.set(this.current_server_trying_to_auth,"success");
-            
+            this.begin_gathering_bot_data(this.current_server_trying_to_auth);
         }
         else{
             this.auth_status.set(this.current_server_trying_to_auth,"failure")
+            this.connections.get(this.current_server_trying_to_auth).close();
+            this.connections.delete(this.current_server_trying_to_auth);
+            this.connection_strings.delete(this.current_server_trying_to_auth);
         }
     }
 
+    begin_gathering_bot_data(server_name:string){
+        try{
+            if(this.connections.has(server_name)){
+               let connection = this.connections.get(server_name);
+               connection.onmessage = this.gather_bot_data;
+            }
+            else{
+                throw new Error("Issue");
+            }
+        }
+        catch{
+            this.auth_status.set(server_name,"failure");
+            this.connections.delete(this.current_server_trying_to_auth);
+            this.connection_strings.delete(this.current_server_trying_to_auth);
+        }
+        
+    
+    }
+
+    gather_bot_data(event:MessageEvent){
+        try{
+            let bot_string = event.data;
+            let bot_object = JSON.parse(bot_string);
+            let server_name = bot_object["server_name"] ;
+            if(server_name == null){
+                throw new Error("issue");
+            }
+            else{
+                //overwrite the old bot string string.
+                this.set_bot_state(prevState => {
+                    let previous = Object.assign({}, prevState);
+                    previous[server_name] = bot_string;                 
+                    return previous;
+                  })
+            }
+        }
+        catch{
+            //alert the 
+        }
+    }
 
     has_credentials(server_name:string):boolean{
         if(this.connections.has(server_name) && this.name_and_type != null){
@@ -67,5 +113,7 @@ export class Client{
         this.name_and_type = name_and_type
     }
 
-
+    define_bot_state(fun:any){
+        this.set_bot_state = fun;
+    }
 }
