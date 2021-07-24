1cb102e0c08796351dfaa57880289074655b6f8c
RonaldColyar <phantexcreations@gmail.com>
2021-07-23 20:57:09 -0500

    init

--- Frontend/src/Components/ServerSettingsDialog.tsx
+++ Frontend/src/Components/ServerSettingsDialog.tsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class ServerSettingsDialog extends Component {
     render() {
         return (
-            <div>
+            <div className = "popup-wrapper" id = "settings-dialog">
+                <h1>Configure Server Settings</h1>
                 
             </div>
         )
     }
 }
+//deactivating
+//activating
+//disconnecting
+//viewing-state
---
+++ Frontend/src/Components/SettingsOption.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SettingsOption extends Component<any,any> {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
