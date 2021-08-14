2488551fdfd69df7da18924c02f0b15b3ca382c7
RonaldColyar <phantexcreations@gmail.com>
2021-08-14 00:50:54 -0500

    basic state listing

---
+++ Frontend/src/Components/BasicStateListing.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BasicStateListing extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
