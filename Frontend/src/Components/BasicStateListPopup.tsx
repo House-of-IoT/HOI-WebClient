2a62ac268da16858491a5fa7785e39e9cc45421f
RonaldColyar <phantexcreations@gmail.com>
2021-08-14 00:50:48 -0500

    basic state listing

--- Frontend/src/Components/BasicStateListPopup.tsx
+++ Frontend/src/Components/BasicStateListPopup.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
-export default class BasicStateListPopup extends Component {
+export default class BasicStateListPopup extends Component<any,any> {
     render() {
         return (
-            <div>
-                
+            <div id = "basic-state-list">
+                <h1>{this.props.name}</h1>
+                <div id = "basic-state-data">
+
+                </div>
+
             </div>
         )
     }
