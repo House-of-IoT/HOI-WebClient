907bd98f235bcec4ab0af9b518cd660077fc19a4
RonaldColyar <phantexcreations@gmail.com>
2021-08-14 06:24:24 -0500

    wip for the night

--- Frontend/src/Components/BasicStateListPopup.tsx
+++ Frontend/src/Components/BasicStateListPopup.tsx
@@ -1,6 +1,31 @@
 import React, { Component } from 'react'
+import BasicStateListing from './BasicStateListing'
 
 export default class BasicStateListPopup extends Component<any,any> {
+
+    map_elements(){
+        let parsed_data = JSON.parse(this.props.data);
+        //array of data
+        if (this.props.type == "Deactivated" || this.props.type == "BannedIps"){
+            parsed_data.map((data:any)=>{
+                return(
+                    <BasicStateListing data = {data}/>
+                )
+            })
+        }
+        else{
+            let keys = Object.keys(parsed_data);
+            //key value pair of data.
+            //formatting name and type as the data for the contact listing
+            keys.map((key:any)=>{
+                let data_string = `${key}(${parsed_data[key]})`
+                return(
+                    <BasicStateListing data = {data_string}/>
+                )
+            })
+        }
+    }
+
     render() {
         return (
             <div id = "basic-state-list">
@@ -13,3 +38,5 @@ export default class BasicStateListPopup extends Component<any,any> {
         )
     }
 }
+
+
