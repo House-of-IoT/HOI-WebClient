b2e565660cc9e491bce0c8823cce5ce963ceefc1
RonaldColyar <PhantexCreations@gmail.com>
2021-09-28 15:29:39 -0500

    motion detection camera further dev

--- Frontend/src/Components/MotionDetectionCamera.tsx
+++ Frontend/src/Components/MotionDetectionCamera.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
-export default class MotionDetectionCamera extends Component {
+export default class MotionDetectionCamera extends Component<any,any> {
     render() {
         return (
-            <div>
-                
+            <div className = "MotionDetectionCamera">
+                <h1>{this.props.bot.name}</h1>
+                <h3>Last Detection:{this.props.bot.last_detection}</h3>
+                <button>Deactivate</button>
+                <button>View Recent Detections</button>
             </div>
         )
     }
