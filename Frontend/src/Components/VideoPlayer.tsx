a359b692516a14edcc26fec772c94b3aef8055a2
RonaldColyar <phantexcreations@gmail.com>
2021-10-19 21:38:23 -0500

    frontend

--- Frontend/src/Components/VideoPlayer.tsx
+++ Frontend/src/Components/VideoPlayer.tsx
@@ -11,7 +11,8 @@ export default class VideoPlayer extends Component<any,any> {
     render() {
         return (
             <div className = "popup-wrapper" id = "video_player">
-                <video src = {this.state.video}>
+                <h2>{this.props.title}</h2>
+                <video src = {this.props.video}>
                 </video>
             </div>
         )
