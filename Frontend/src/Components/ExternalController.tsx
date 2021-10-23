f1a245c26e0d1d4f74f87b52e3e4f7d4ac58f00f
RonaldColyar <PhantexCreations@gmail.com>
2021-10-23 03:28:54 -0500

    external controller listing init

--- Frontend/src/Components/ExternalController.tsx
+++ Frontend/src/Components/ExternalController.tsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 export default class ExternalController extends Component {
     render() {
         return (
-            <div>
+            <div id = "ExternalControllerViewWrapper">
+                <h2>External Controller</h2>
+                <button>Add Connection</button>
                 
+                <h3>Connections:</h3>
+                <div id = "ExternalControllerConnections">
+
+
+                </div>
+
             </div>
         )
     }
