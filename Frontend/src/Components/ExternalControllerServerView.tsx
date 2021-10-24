6912cb4e690782c3f9625108edf6a2d72f1b107f
RonaldColyar <PhantexCreations@gmail.com>
2021-10-24 00:50:51 -0500

    external controller server view components(stage1)

---
+++ Frontend/src/Components/ExternalControllerServerView.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ExternalControllerServerView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
