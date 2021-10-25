b1253a29cc3135230b0ce9fbc76e20c26cdfae39
RonaldColyar <PhantexCreations@gmail.com>
2021-10-24 21:17:48 -0500

    init

---
+++ Frontend/src/Components/ExternalControllerRelationView.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ExternalControllerRelationView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
