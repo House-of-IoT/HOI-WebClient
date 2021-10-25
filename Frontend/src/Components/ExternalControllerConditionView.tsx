99108a0201eac57b631892e7bf3bf81a710853ae
RonaldColyar <PhantexCreations@gmail.com>
2021-10-24 22:52:39 -0500

    fill in for comps needed for external controlling

--- Frontend/src/Components/ExternalControllerConditionView.tsx
+++ Frontend/src/Components/ExternalControllerConditionView.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
-export default class ExternalControllerConditionView extends Component {
+export default class ExternalControllerConditionView extends Component<any,any> {
     render() {
         return (
-            <div>
-                
+            <div className = "condition-view">
+                <h1>{this.props.key_name}</h1>
+                <h1>{this.props.key_value}</h1>
+                <button>Edit</button>
             </div>
         )
     }
