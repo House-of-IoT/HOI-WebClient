702203226cb4b6e269d862edacba4dce75e33599
RonaldColyar <PhantexCreations@gmail.com>
2021-10-25 00:07:00 -0500

    more progression with controller relation view

--- Frontend/src/Components/ExternalControllerRelationView.tsx
+++ Frontend/src/Components/ExternalControllerRelationView.tsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import ExternalControllerConditionView from './ExternalControllerConditionView';
 
 export default class ExternalControllerRelationView extends Component<any,any>{
     render() {
-        let conditions = Object.keys(this.props.relation.conditions)
-
+        let conditions: Array<string> = Object.keys(this.props.relation.conditions);
+        
         return (
             <div className = "relation-view-box">
                 <h2>{this.props.relation.device_name}</h2>
                 <h2>{this.props.relation.action}</h2>
-                <div>
+                <div className = "conditions-box">
+                    {conditions.map((key:any)=>{
+                        return (
+                        <ExternalControllerConditionView key_name = {key} key_value = {this.props.relation.conditions[key]}/>);
 
+                    })}
                 </div>
 
             </div>
