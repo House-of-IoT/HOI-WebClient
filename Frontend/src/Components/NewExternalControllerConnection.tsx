82c0aa3f9ee14c085fb5d1c2d99a985d7cf11c11
RonaldColyar <PhantexCreations@gmail.com>
2021-10-26 19:57:14 -0500

    init

--- Frontend/src/Components/NewExternalControllerConnection.tsx
+++ Frontend/src/Components/NewExternalControllerConnection.tsx
@@ -4,7 +4,11 @@ export default class NewExternalControllerConnection extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Add External Controller Connection</h1>
+                <input placeholder= "host"></input>
+                <input placeholder= "port"></input>
+                <input placeholder= "display name"></input>
+                <input placeholder = "name on server"></input>
             </div>
         )
     }
