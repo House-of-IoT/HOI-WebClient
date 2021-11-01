1df26b84397d424018ad9577c466512b90f23194
RonaldColyar <PhantexCreations@gmail.com>
2021-11-01 08:55:43 -0500

    styling for conditions

--- Frontend/src/Components/ExternalControllerConditionView.tsx
+++ Frontend/src/Components/ExternalControllerConditionView.tsx
@@ -4,9 +4,14 @@ export default class ExternalControllerConditionView extends Component<any,any> 
     render() {
         return (
             <div className = "condition-view">
-                <h1>{this.props.key_name}</h1>
-                <h1>{this.props.key_value}</h1>
-                <button>Edit</button>
+                <h3 className = "condition-keys">KEY:{this.props.key_name}</h3>
+                <h3 className = "condition-values">VALUE:{this.props.key_value}</h3>
+                <button 
+                    id = "condition-edit-button" 
+                    className = "condition-modify-buttons">Edit</button>
+                <button
+                    id = "condition-remove-button" 
+                    className = "condition-modify-buttons">Remove</button>
             </div>
         )
     }
--- Frontend/src/Components/ExternalControllerRelationView.tsx
+++ Frontend/src/Components/ExternalControllerRelationView.tsx
@@ -7,13 +7,15 @@ export default class ExternalControllerRelationView extends Component<any,any>{
         
         return (
             <div className = "relation-view-box">
-                <h2>{this.props.relation.device_name}</h2>
-                <h2>{this.props.relation.action}</h2>
+                <h2>Device name:{this.props.relation.device_name}</h2>
+                <h2>Action:{this.props.relation.action}</h2>
+                <h3 id = "relation-view-condition-tag">Conditions:</h3>
                 <div className = "conditions-box">
+                    
                     {conditions.map((condition:any)=>{
                         let key = Object.keys(condition)[0];
                         return (
-                        <ExternalControllerConditionView key_name = {key} key_value = {this.props.relation.conditions[key]}/>);
+                        <ExternalControllerConditionView key_name = {key} key_value = {condition[key]}/>);
 
                     })}
                 </div>
