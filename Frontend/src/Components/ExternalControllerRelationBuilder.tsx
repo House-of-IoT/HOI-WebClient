ede92181ffbc109c5bed3118dc9d52bbcc7a5e00
RonaldColyar <PhantexCreations@gmail.com>
2021-11-02 08:40:46 -0500

    added external controller relation builder structure

--- Frontend/src/Components/ExternalControllerRelationBuilder.tsx
+++ Frontend/src/Components/ExternalControllerRelationBuilder.tsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
 
-interface Condition{
-    key:String,
-    value:String
-}
+
 
 export default class ExternalControllerRelationBuilder extends Component<any,any> {
 
     constructor(){
         super(null);
         this.state = {
-            added_conditions: new Array<Condition>(),
+            added_conditions: new Map<string,string>(),
             current_action :"",
             current_device_name:"",
             current_key :"",
@@ -34,7 +31,9 @@ export default class ExternalControllerRelationBuilder extends Component<any,any
                     <input placeholder = "Device Name" onInput = 
                         {
                             (event:React.FormEvent<HTMLInputElement>)=>
-                            {this.setState({current_device_name:event.currentTarget.value})}}></input>
+                            {this.setState({current_device_name:event.currentTarget.value})}}>
+
+                    </input>
                     
                     
                     <div id = "add-relation-condition-wrapper">
@@ -53,29 +52,37 @@ export default class ExternalControllerRelationBuilder extends Component<any,any
                         </input>
 
                         <button onClick = {()=>{this.setState((prev)=>{
-                            let new_condition : Condition = {key:this.state.current_key,value:this.state.current_value}
-                            prev.added_conditions.push(new_condition);
-                            return prev;
+                            let previous = Object.assign({}, prev);
+                            previous.added_conditions.set(this.state.current_key,this.state.current_value)
+                            return previous;
                         })}}
                         
                             >Create Condition</button>
                     </div>
 
                     <h3>Conditions Added So Far:</h3>
+                    
                     <div id = "added-conditions-relation-builder">
-                        {this.state.added_relations.map((relation)=>{
+                        {Object.keys(this.state.added_relations).map((key)=>{
                             return(
 
                                 <div className = "added-conditions">
-                                    <h3>KEY:{relation.key}</h3>
-                                    <h3>VALUE:{relation.value}</h3>
-                                    <button>Remove</button>
+                                    <h3>KEY:{key}</h3>
+                                    <h3>VALUE:{this.state.added_conditions.get(key)}</h3>
+                                    <button 
+                                        onClick = {()=>{this.setState((prev)=>{
+                                            let previous = Object.assign({}, prev);
+                                            previous.added_conditions.delete(key);
+                                            return previous;
+                                        })}}
+                                    >Remove</button>
                                 </div>
                             );
 
                         })}
                     </div>
-                
+                    <button>Add Relation</button>
+                    <button>Close</button>
                 </div>                
             </div>
         )
