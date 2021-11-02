ef30de65a74ec0427eec3c0a1252f3cc4561142c
RonaldColyar <PhantexCreations@gmail.com>
2021-11-02 04:29:55 -0500

    styling for server view/conditions

--- Frontend/src/Components/ExternalControllerListing.tsx
+++ Frontend/src/Components/ExternalControllerListing.tsx
@@ -11,7 +11,8 @@ export default class ExternalControllerListing extends Component<any,any> {
                     let previous = Object.assign({}, prevState);
                     previous.selected_external_controller = this.props.name;
                     previous.external_controller_server_view_showing = true;
-                    previous.external_controller_relations.set("test1",JSON.stringify([{device_name : "tester",action:"test2",conditions:[{test21:"2"},{test212:"2333"}]}]));
+                    previous.external_controller_relations.set("test1",JSON.stringify([{device_name : "tester",action:"test2",conditions:[{test21:"2"},{test212:"2333"}]},
+                    {device_name : "tester2",action:"test22",conditions:[{test212:"2"},{test1212:"23323"}]}]));
                     return previous;
                     });
                                
